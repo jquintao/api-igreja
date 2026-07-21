2f16d2b90724b5fc324ac77c94a0cdb9568a36b8
jquintao <47524964+jquintao@users.noreply.github.com>
2026-07-21 14:03:47 -0300

    Update models.py

--- models.py
+++ models.py
@@ -20,3 +20,23 @@ class Membro(Base):
     created_date = Column(TIMESTAMP, server_default=func.now())
     updated_by = Column(Integer)
     update_date = Column(TIMESTAMP, onupdate=func.now())
+    
+# Adicione no final do models.py
+class Visitante(Base):
+    __tablename__ = "visitantes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, nullable=False)
+    idade = Column(Integer, nullable=True)
+    sexo = Column(String, nullable=True)
+    profissao = Column(String, nullable=True)
+    data_nasc = Column(Date, nullable=True)
+    telefone = Column(String, nullable=True)
+    endereco = Column(String, nullable=True)
+    data_visita = Column(Date, nullable=True)
+    
+    # Campos de Auditoria
+    create_by = Column(Integer, nullable=False)
+    created_date = Column(DateTime, default=func.now())
+    updated_by = Column(Integer, nullable=True)
+    update_date = Column(DateTime, nullable=True, onupdate=func.now())
