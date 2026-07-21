81463a9bc4bcf22128fc7c2b7cd22c541f6a3dfc
jquintao <47524964+jquintao@users.noreply.github.com>
2026-07-21 14:05:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,18 @@ def create_membro(membro: schemas.MembroCreate, db: Session = Depends(get_db)):
 @app.get("/membros/", response_model=List[schemas.MembroResponse])
 def read_membros(db: Session = Depends(get_db)):
     return db.query(models.Membro).all()
+
+# Adicione junto com as outras rotas no main.py
+
+@app.post("/visitantes/", response_model=schemas.VisitanteResponse)
+def create_visitante(visitante: schemas.VisitanteCreate, db: Session = Depends(get_db)):
+    db_visitante = models.Visitante(**visitante.model_dump()) # ou .dict() se a versão do pydantic for mais antiga
+    db.add(db_visitante)
+    db.commit()
+    db.refresh(db_visitante)
+    return db_visitante
+
+@app.get("/visitantes/", response_model=list[schemas.VisitanteResponse])
+def read_visitantes(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    visitantes = db.query(models.Visitante).offset(skip).limit(limit).all()
+    return visitantes
